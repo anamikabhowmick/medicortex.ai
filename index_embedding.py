e9c81fdad095fb282ce5ea19a59639ef692f7fe4
Indranil-Seal <indranil.seal09@gmail.com>
2025-09-14 12:11:24 +0530

    integration of the embedding layer - tested OK

--- index_embedding.py
+++ index_embedding.py
@@ -4,11 +4,6 @@ from sentence_transformers import SentenceTransformer
 import faiss
 import openai
 
-# Step 2: Load and preprocess data
-df = pd.read_csv(r"D:\Data & Projects\medicortex.ai\data\\rawdata.csv")
-df['text'] = df['Patient'].astype(str) + " " + df['Description'].astype(str)
-print(df)
-
 ## testing embedding:
 def sentence_embedding(df, column_name):
     """
@@ -27,15 +22,3 @@ def sentence_embedding(df, column_name):
     # Generate embeddings
     embeddings = model.encode(sentences, show_progress_bar=True)
     return embeddings
-
-# For 'Description' column:# For 'Patient' column:
-desc_embeddings = sentence_embedding(df, 'Description')
-print(desc_embeddings)
-
-# For 'Patient' column:
-patient_embeddings = sentence_embedding(df, 'Patient')
-print(patient_embeddings)
-
-# For 'Doctor' column:
-doctor_embeddings = sentence_embedding(df, 'Doctor')
-print(doctor_embeddings)
--- pipeline.py
+++ pipeline.py
@@ -1,16 +1,35 @@
-python
-
-#%%
 # call libraries:
 import pandas as pd
 from pathlib import Path
 from typing import Union
+
+import sentence_transformers
+from sentence_transformers import SentenceTransformer
+import faiss
+import openai
+
 
 
 # call modules & methods:
 import dataprocessing
 from dataprocessing import *
 
+import index_embedding
+from index_embedding import *
+
 # pipeline:
-input_load = "D:\Data & Projects\medicortex.ai\data\\rawdata.csv"
+input_load = r"D:\Data & Projects\medicortex.ai\data\\rawdata.csv"
 data = load_and_process_data(input_load)
+
+# Run index embedding for all conversation attributes
+# 'Description' column:
+desc_embeddings = sentence_embedding(data, 'Description')
+print(desc_embeddings)
+
+# 'Patient' column:
+patient_embeddings = sentence_embedding(data, 'Patient')
+print(patient_embeddings)
+
+# 'Doctor' column:
+doctor_embeddings = sentence_embedding(data, 'Doctor')
+print(doctor_embeddings)
